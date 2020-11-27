5183fd32519c9da0b2ea0e6b0890931efcc673e7
puhanjie <1318327104@qq.com>
2020-11-27 21:54:00 +0800

    restful接口风格中，get，post，put，delete四种请求的编写

--- src/main/java/com/puhj/springbootdemo/api/DeptApi.java
+++ src/main/java/com/puhj/springbootdemo/api/DeptApi.java
@@ -3,13 +3,11 @@ package com.puhj.springbootdemo.api;
 import com.puhj.springbootdemo.entity.Dept;
 import com.puhj.springbootdemo.service.DeptService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/*该controller主要演示restful接口风格，get，post，put，delete四种请求的写法*/
 @RestController("deptApi")
 @RequestMapping("/dept")
 public class DeptApi {
@@ -26,4 +24,20 @@ public class DeptApi {
     public Dept get(@PathVariable("id") Integer id) {
         return deptService.get(id);
     }
+
+    @PostMapping("/add")
+    public void add(Dept dept) {
+        deptService.add(dept);
+    }
+
+    @PutMapping("/edit")
+    public void edit(Dept dept) {
+        deptService.edit(dept);
+    }
+
+    @DeleteMapping("/remove")
+    public void remove(Integer id) {
+        deptService.remove(id);
+    }
+
 }
